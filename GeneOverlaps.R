ce77630496839d885585b5771b8c9b57df30b8e7
Md Maqsood Ahamad Khan <122003574+bioinfomak@users.noreply.github.com>
2026-01-09 14:20:41 +0530

    Update GeneOverlaps.R

--- GeneOverlaps.R
+++ GeneOverlaps.R
@@ -3,61 +3,101 @@ library(GenomicFeatures)
 library(AnnotationDbi)
 library(org.Hs.eg.db)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(VariantAnnotation)
+library(GenomicRanges)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(org.Hs.eg.db)
 
-#----1) convert filtered VCF File to output.filtered VCF -----
-setwd ("D:\ALS_26122025\BAM")
-list.files()
-# Load the VCF file
-vcf_file <- "SS91_filtered_variants.vcf"
-vcf <- readVcf(vcf_file, genome = "hg38")
-vcf_filter<- vcf[(vcf@fixed$FILTER=="PASS"),]
-# Save the VCF file
-writeVcf(vcf_filter, "SS91_output_filterd.vcf")
+setwd("PATH")
 
-# --- 1) Load your VCF ---
-list.files()
-vcf_file <- "SS91_output_filterd.vcf"
-vcf <- readVcf(vcf_file, genome="hg38")
-gr_variants <- rowRanges(vcf)  # variant GRanges
-info(vcf)
-# --- 2) Load gene annotation from TxDb ---
+# List all filtered VCFs
+vcf_files <- list.files(pattern = "_filtered_variants\\.vcf$")
+
+# Loop over each VCF
+for (vcf_file in vcf_files) {
+  
+  message("Processing: ", vcf_file)
+  
+  # Read VCF
+  vcf <- readVcf(vcf_file, genome = "hg38")
+  
+  # Keep only PASS variants
+  vcf_pass <- vcf[fixed(vcf)$FILTER == "PASS", ]
+  
+  # Define output name
+  out_file <- sub("_filtered_variants\\.vcf$", "_output_filtered.vcf", vcf_file)
+  
+  # Write VCF
+  writeVcf(vcf_pass, out_file)
+}
+
+
+############################################################
+## Map variants to genes for multiple VCFs (with variant_id)
+############################################################
+# Input and output folders
+input_dir  <- "PATH"
+output_dir <- "PATH"
+
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+
+setwd(input_dir)
+
+# Load gene annotation once
 gr_genes <- genes(TxDb.Hsapiens.UCSC.hg38.knownGene)
-#cds(TxDb.Hsapiens.UCSC.hg38.knownGene,columns="cds_id", filter=NULL, use.names=FALSE)
-
-# Map ENTREZ IDs to gene symbols
-gene_symbols <- mapIds(org.Hs.eg.db,
-                       keys=as.character(gr_genes$gene_id),
-                       column="SYMBOL",
-                       keytype="ENTREZID",
-                       multiVals="first")
+gene_symbols <- mapIds(
+  org.Hs.eg.db,
+  keys       = as.character(gr_genes$gene_id),
+  column     = "SYMBOL",
+  keytype    = "ENTREZID",
+  multiVals  = "first"
+)
 gr_genes$gene_name <- gene_symbols
 
-# Quick check
-head(gr_genes)
+# Get all filtered VCFs
+vcf_files <- list.files(pattern = "_output_filtered\\.vcf$", full.names = TRUE)
+stopifnot(length(vcf_files) > 0)
 
-# --- 3) Find overlaps ---
-hits <- findOverlaps(gr_variants, gr_genes, ignore.strand=TRUE)
-
-# --- 4) Create overlap table ---
-# Collapse ALT alleles so lengths match
-alt_alleles <- sapply(mcols(gr_variants)$ALT[queryHits(hits)], function(x) {
-  paste(as.character(x), collapse=",")
-})
-
-overlap_table <- data.frame(
-  variant_id = names(gr_variants)[queryHits(hits)],
-  chr        = as.character(seqnames(gr_variants)[queryHits(hits)]),
-  pos        = start(gr_variants)[queryHits(hits)],
-  ref        = as.character(mcols(gr_variants)$REF[queryHits(hits)]),
-  alt        = alt_alleles,
-  gene_id    = gr_genes$gene_id[subjectHits(hits)],
-  gene_name  = gr_genes$gene_name[subjectHits(hits)],
-  stringsAsFactors = FALSE
-)
-
-head(overlap_table)
-
-write.csv(overlap_table,file= "SS91_VCF_Gene.csv")
-
-
-
+for (vcf_file in vcf_files) {
+  message("Processing: ", basename(vcf_file))
+  
+  vcf <- readVcf(vcf_file, genome = "hg38")
+  gr_variants <- rowRanges(vcf)
+  hits <- findOverlaps(gr_variants, gr_genes, ignore.strand = TRUE)
+  
+  if (length(hits) == 0) {
+    message("  No gene overlaps found. Skipping.")
+    next
+  }
+  
+  alt_alleles <- sapply(
+    mcols(gr_variants)$ALT[queryHits(hits)],
+    function(x) paste(as.character(x), collapse = ",")
+  )
+  
+  variant_id_vec <- paste0(
+    as.character(seqnames(gr_variants)[queryHits(hits)]), ":",
+    start(gr_variants)[queryHits(hits)], "_",
+    as.character(mcols(gr_variants)$REF[queryHits(hits)]), "/",
+    alt_alleles
+  )
+  
+  overlap_table <- data.frame(
+    variant_id = variant_id_vec,
+    chr        = as.character(seqnames(gr_variants)[queryHits(hits)]),
+    pos        = start(gr_variants)[queryHits(hits)],
+    ref        = as.character(mcols(gr_variants)$REF[queryHits(hits)]),
+    alt        = alt_alleles,
+    gene_id    = gr_genes$gene_id[subjectHits(hits)],
+    gene_name  = gr_genes$gene_name[subjectHits(hits)],
+    stringsAsFactors = FALSE
+  )
+  
+  out_csv <- file.path(
+    output_dir,
+    sub("_output_filtered\\.vcf$", "_VCF_Gene.csv", basename(vcf_file))
+  )
+  
+  write.csv(overlap_table, out_csv, row.names = FALSE)
+  message("  Saved: ", out_csv)
+}
